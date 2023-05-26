5ca0938e933949d1248f8b3ec4cce7da6bcbc94b
Nathan Moebus <nathan.moebus@wolterskluwer.com>
2023-05-26 14:35:55 -0400

    init

---
+++ MainPage.xaml.cs
@@ -0,0 +1,16 @@
+﻿namespace MauiApp1
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+            var mapControl = new Mapsui.UI.Maui.MapControl();
+            mapControl.Map?.Layers.Add(Mapsui.Tiling.OpenStreetMap.CreateTileLayer());
+            Content = mapControl;
+        }
+
+    }
+}
